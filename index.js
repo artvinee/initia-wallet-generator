1670e9c5accaa0e0bc9e9db9016be5d168106e03
artvine <74613662+artvinee@users.noreply.github.com>
2024-05-24 19:47:22 +0400

    remove extractWalletAddresses

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const readline = require('readline');
-const figlet = require("figlet")
-const { extractWalletAddresses } = require('./src/extractWalletAddresses');
+const figlet = require("figlet");
 const { generateWallets } = require('./src/generateWallets');
 
-// Read user input
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
@@ -15,27 +13,13 @@ function askQuestion(query) {
 
 (async () => {
     const banner = await figlet.text("Initia Gen");
-    console.log(banner, "\nGithub: https://github.com/Tnodes\n");
-    console.log('Menu:');
-    console.log('1. Generate New Wallets');
-    console.log('2. Extract & Merge Wallet Addresses from File');
-    const choice = await askQuestion('Choose an option (1 or 2): ');
+    console.log(banner, "\nGithub: https://github.com/Tnodes (modded by artvinee)\n");
 
-    if (choice === '1') {
-        const numWallets = parseInt(await askQuestion('How many wallets do you want to generate? '));
-        if (isNaN(numWallets) || numWallets <= 0) {
-            console.log('Please enter a valid number of wallets.');
-        } else {
-            const filePath = await askQuestion('Enter the path to save the file (e.g., wallets.txt): ');
-            await generateWallets(numWallets, filePath);
-        }
-    } else if (choice === '2') {
-        const inputFile = await askQuestion('Enter input file name (e.g., wallets.txt): ');
-        const outputFile = await askQuestion('Enter output file name (e.g., list_wallet.txt): ');
-
-        await extractWalletAddresses(inputFile, outputFile);
+    const numWallets = parseInt(await askQuestion('How many wallets do you want to generate?'));
+    if (isNaN(numWallets) || numWallets <= 0) {
+        console.log('Please enter a valid number of wallets.');
     } else {
-        console.log('Invalid choice.');
+        await generateWallets(numWallets);
     }
 
     rl.close();
